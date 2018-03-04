82918aa0915b5afe295b82fe3d5dbc7e87d8089e
kurisu <18581489108@163.com>
2018-03-04 21:31:10 +0800

    初始化项目

---
+++ FlyingGameClient/Assets/Scripts/MoveController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour {
+
+    // 主体
+    GameObject mainBody;
+
+    public Transform forceHead;
+
+    public Transform forceLeftAirfoil;
+    public Transform forceRightAirfoil;
+
+    public Transform forceLeftTailAirfoil;
+    public Transform forceRightTailAirfoil;
+
+    private Transform thisTransform;
+    private Rigidbody thisRigidbody;
+
+    public float speed = 250.0f;
+
+    public float airfoilForce = 11.0f;
+
+    public float tailAirfoilForce = 6.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        mainBody = this.gameObject;
+        thisTransform = transform;
+
+        /*
+        forceHead = thisTransform.Find("FlyingVehicle/ForcePoints/ForceHead");
+
+        forceLeftAirfoil = thisTransform.Find("FlyingVehicle/ForcePoints/ForceLeftAirfoil");
+        forceRightAirfoil = thisTransform.Find("FlyingVehicle/ForcePoints/ForceRightAirfoil");
+
+        forceLeftTailAirfoil = thisTransform.Find("FlyingVehicle/ForcePoints/ForceLeftTailAirfoil");
+        forceRightTailAirfoil = thisTransform.Find("FlyingVehicle/ForcePoints/ForceRightTailAirfoil");
+        */
+        thisRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        thisRigidbody.AddForceAtPosition(thisTransform.forward * speed, forceHead.position);
+
+        thisRigidbody.AddForceAtPosition(thisTransform.up * airfoilForce, forceLeftAirfoil.position);
+        thisRigidbody.AddForceAtPosition(thisTransform.up * airfoilForce, forceRightAirfoil.position);
+
+        thisRigidbody.AddForceAtPosition(thisTransform.up * tailAirfoilForce, forceLeftTailAirfoil.position);
+        thisRigidbody.AddForceAtPosition(thisTransform.up * tailAirfoilForce, forceRightTailAirfoil.position);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            // 俯冲
+            thisRigidbody.AddForceAtPosition(thisTransform.up * tailAirfoilForce, forceLeftTailAirfoil.position);
+            thisRigidbody.AddForceAtPosition(thisTransform.up * tailAirfoilForce, forceRightTailAirfoil.position);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            // 爬升
+            thisRigidbody.AddForceAtPosition(thisTransform.up * -tailAirfoilForce, forceLeftTailAirfoil.position);
+            thisRigidbody.AddForceAtPosition(thisTransform.up * -tailAirfoilForce, forceRightTailAirfoil.position);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            // 左翻滚
+            thisRigidbody.AddForceAtPosition(thisTransform.up * -tailAirfoilForce, forceLeftTailAirfoil.position);
+            thisRigidbody.AddForceAtPosition(thisTransform.up * tailAirfoilForce, forceRightTailAirfoil.position);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            // 右翻滚
+            thisRigidbody.AddForceAtPosition(thisTransform.up * tailAirfoilForce, forceLeftTailAirfoil.position);
+            thisRigidbody.AddForceAtPosition(thisTransform.up * -tailAirfoilForce, forceRightTailAirfoil.position);
+        }
+    } 
+}
---
+++ FlyingGameClient/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour {
+    // 摄像机与目标的水平距离
+    public float horizontalDistance;
+
+    // 摄像机与目标的垂直距离
+    public float verticalDistance;
+
+    // 摄像机移动到目标点的平滑度，也可以理解为速度
+    public float smooth;
+
+    // 跟随的目标
+    public Transform target;
+ 
+	
+	void LateUpdate () {
+        Vector3 targetPos = target.position + Vector3.up * verticalDistance + target.forward * horizontalDistance;
+
+        this.transform.position = Vector3.Lerp(this.transform.position, targetPos, Time.deltaTime * smooth);
+
+        this.transform.LookAt(target);
+	}
+}
