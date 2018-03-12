966196c914b7321b296bdfe8949346da95c8479e
kurisu <18581489108@163.com>
2018-03-12 23:09:49 +0800

    基础常量

---
+++ FlyingGameClient/Assets/Scripts/Flight/InputConstans.cs
@@ -0,0 +1,13 @@
+﻿namespace Flight
+{
+    public class InputConstans
+    {
+        public const string MFB = "Vertical";
+        public const string RLR = "Horizontal";
+        public const string MUD = "MUD";
+        public const string MLR = "MLR";
+
+        public const string SLR = "SLR";
+        public const string SUD = "SUD";
+    }
+}
