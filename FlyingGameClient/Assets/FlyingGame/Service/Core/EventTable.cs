49155e803c43074a46c2062777d94e102301bb88
kurisu <18581489108@163.com>
2018-03-17 23:37:33 +0800

    实现ModuleEvent

---
+++ FlyingGameClient/Assets/FlyingGame/Service/Core/EventTable.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+namespace FlyingGame.Service.Core
+{
+    public class ModuleEvent : UnityEvent<object>
+    {
+
+    }
+
+    public class ModuleEvent<T> : UnityEvent<T>
+    {
+
+    }
+
+    public class EventTable
+    {
+        private Dictionary<string, ModuleEvent> m_mapEvents;
+
+        /// <summary>
+        /// 获取type对应的一个ModuleEvent
+        /// 如果不存在，则实例化一个ModuleEvent
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public ModuleEvent GetEvent(string type)
+        {
+            if (m_mapEvents == null)
+                m_mapEvents = new Dictionary<string, ModuleEvent>();
+
+            if (!m_mapEvents.ContainsKey(type))
+                m_mapEvents.Add(type, new ModuleEvent());
+
+            return m_mapEvents[type];
+        }
+
+        public void clear()
+        {
+            if (m_mapEvents == null)
+                return;
+
+            foreach (var @event in m_mapEvents)
+            {
+                @event.Value.RemoveAllListeners();
+            }
+
+            m_mapEvents.Clear();
+        }
+    }
+}
+
+
---
+++ FlyingGameClient/Assets/FlyingGame/Service/Core/Module.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using SGF;
+
+namespace FlyingGame.Service.Core
+{
+    public abstract class Module
+    {
+        public virtual void Release()
+        {
+            this.Log("Release");
+        }
+    }
+}
+
